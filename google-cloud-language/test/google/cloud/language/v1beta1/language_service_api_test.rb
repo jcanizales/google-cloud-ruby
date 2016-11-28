6e741d54b5c4fe9396f0615153fcbe8988b25f09
Jorge Canizales <jcanizales@google.com>
2016-11-28 02:06:04 -0800

    Handwritten test for Language API

---
+++ google-cloud-language/test/google/cloud/language/v1beta1/language_service_api_test.rb
@@ -0,0 +1,125 @@
+# Copyright 2016 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "minitest/autorun"
+require "google/cloud/language/v1beta1/language_service_api"
+include Google::Cloud::Language::V1beta1
+
+describe LanguageServiceApi do
+
+  def stub_grpc_client
+    mock_client = Minitest::Mock.new
+
+    # Implement "method" method for the mock.
+    def mock_client.method(name_symbol)
+      # We return a lambda (an instance of Proc) instead of a Method, because
+      # the latter can't be easily constructed on the fly. In practice, most
+      # operations performed on Methods work on Procs. The differences are:
+      # Proc implements: :yield, :lambda?, :binding
+      # Method implements: :receiver, :name, :original_name, :owner, :unbind,
+      #                    and :super_method
+      lambda { |*args| public_send(name_symbol, *args) }
+    end
+
+    # TODO: Instead of mock_client, pass a block that verifies the host string
+    # and that the 2nd argument isn't :this_channel_is_insecure
+    LanguageService::Stub.stub(:new, mock_client) { yield(mock_client) }
+  end
+
+  describe "#analyze_sentiment" do
+    it "forwards calls to LanguageService::Stub" do
+      stub_grpc_client do |mock_client|
+        expected_request = AnalyzeSentimentRequest.new(
+          document: Document.new,
+        )
+
+        mocked_response = AnalyzeSentimentResponse.new
+
+        mock_client.expect(
+          :analyze_sentiment,
+          mocked_response,
+          [expected_request, Hash],
+        )
+
+        language_service_api = LanguageServiceApi.new
+        document = Document.new
+        response = language_service_api.analyze_sentiment(document)
+
+        # Verification:
+        response.must_equal mocked_response
+      end
+    end
+  end
+
+  describe "#analyze_entities" do
+    it "forwards calls to LanguageService::Stub" do
+      stub_grpc_client do |mock_client|
+        expected_request = AnalyzeEntitiesRequest.new(
+          document: Document.new,
+          encoding_type: EncodingType::NONE,
+        )
+
+        mocked_response = AnalyzeEntitiesResponse.new
+
+        mock_client.expect(
+          :analyze_entities,
+          mocked_response,
+          [expected_request, Hash],
+        )
+
+        language_service_api = LanguageServiceApi.new
+        document = Document.new
+        encoding_type = EncodingType::NONE
+        response = language_service_api.analyze_entities(document, encoding_type)
+
+        # Verification:
+        response.must_equal mocked_response
+      end
+    end
+  end
+
+  describe "#annotate_text" do
+    it "forwards calls to LanguageService::Stub" do
+      stub_grpc_client do |mock_client|
+        expected_request = AnnotateTextRequest.new(
+          document: Document.new,
+          features: AnnotateTextRequest::Features.new,
+          encoding_type: EncodingType::NONE,
+        )
+
+        mocked_response = AnnotateTextResponse.new
+
+        mock_client.expect(
+          :annotate_text,
+          mocked_response,
+          [expected_request, Hash],
+        )
+
+        # From the sample:
+        # TODO(jcanizales): Initialize enums with the second value, strings with
+        # "sample", arrays with 1 element, etc. Do the same with the mocked
+        # response.
+        # TODO(jcanizales): Rename to "api" in the sample?
+        language_service_api = LanguageServiceApi.new
+        document = Document.new
+        features = AnnotateTextRequest::Features.new
+        encoding_type = EncodingType::NONE
+        response = language_service_api.annotate_text(document, features, encoding_type)
+
+        # Verification:
+        response.must_equal mocked_response
+      end
+    end
+  end
+end
